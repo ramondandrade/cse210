904159be5d9ab3cf469c87bdf8744e81e4342cc0
Ramon Andrade <tudonetrn@gmail.com>
2025-06-07 08:06:51 +0200

    week 5 - homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        // Test base Assignment
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Test MathAssignment
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment w1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+        
     }
 }
